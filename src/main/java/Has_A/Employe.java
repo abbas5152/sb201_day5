115cea68c91669ed40f2f3383c39a2dc5650da21
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-09-09 22:57:52 +0530

    Add files via upload

---
+++ src/main/java/Has_A/Employe.java
@@ -0,0 +1,102 @@
+package sb201_day5_hasA;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Entity
+public class Employe {
+
+	
+	
+	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+		private int empID;
+		private String name;
+		private String gender;
+		private int salary;
+		
+		
+
+		@ElementCollection
+		@Embedded
+		private Set<Address> empAddress = new HashSet();
+
+
+
+		public int getEmpID() {
+			return empID;
+		}
+
+
+
+		public void setEmpID(int empID) {
+			this.empID = empID;
+		}
+
+
+
+		public String getName() {
+			return name;
+		}
+
+
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+
+
+		public String getGender() {
+			return gender;
+		}
+
+
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+
+
+		public int getSalary() {
+			return salary;
+		}
+
+
+
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+
+
+
+		public Set<Address> getEmpAddress() {
+			return empAddress;
+		}
+
+
+
+		public void setEmpAddress(Set<Address> empAddress) {
+			this.empAddress = empAddress;
+		}
+
+
+
+		@Override
+		public String toString() {
+			return "Employee [empID=" + empID + ", name=" + name + ", gender=" + gender + ", salary=" + salary
+					+ ", empAddress=" + empAddress + "]";
+		}
+		
+		
+		
+
+}
